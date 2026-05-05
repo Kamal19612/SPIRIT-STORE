c03a6a92094a7bc9cdca5a4b8845d02aae95a540
Kamal19612 <ouedraogokamalelise@gmail.com>
2026-05-05 10:37:29 +0000

    initialisation du projetr

---
+++ src/main/java/spirit_design/spirit_store/SpiritStoreApplication.java
@@ -0,0 +1,13 @@
+package spirit_design.spirit_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiritStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiritStoreApplication.class, args);
+	}
+
+}
